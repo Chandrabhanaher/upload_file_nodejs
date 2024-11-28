11e60bbac7a74600c50deed541e281e88ac496d3
Chandrabhan Aher <53216031+Chandrabhanaher@users.noreply.github.com>
2024-11-28 11:23:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const path = require('path')
 const multer = require('multer')
+
+
+const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
 const app = express()
+
+
 
 const storage = multer.diskStorage({
     destination: function(req,file,cd){
@@ -16,17 +23,61 @@ const upload = multer({storage})
 
 app.set('view engine','ejs')
 app.set('views', path.resolve('./views'))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+
+const swaggerDefinition = {
+    openapi: "3.0.0",
+    info: {
+        title: "NodeJS Demo",
+        version: "0.1.0",
+        description: "Upload files",
+    },
+    servers: [
+        {
+            "url": "http://localhost:8000/",
+            "description": "Local Dev"
+        },
+    ],
+}
+const options = {
+    swaggerDefinition,
+    apis: ['./index.js']
+}
+
+// Swagger UI
+const swaggerSpecs = swaggerJsdoc(options)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs))
+
 
 app.get('/', (req,res)=>{
     return res.render('home')
 })
 
-app.post('/profile',upload.single('profilePic'), (req,res)=>{
-    console.log(req.body)
-    console.log(req.file)
 
-    return res.redirect('/')
+/**
+ * @swagger
+ * /profile:
+ *      post:
+ *          summary: Uploads a file.
+ *          discription: Upload file to server
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  multipart/form-data:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              fileName:
+ *                                  type: string
+ *                                  format: binary
+ *          responses:
+ *             200:
+ *                 description: "File uploaded successfully"       
+ */
+
+app.post('/profile',upload.single('fileName'), (req,res)=>{
+    return res.status(200).json({ status: 'File uploaded successfully' })
 })
 
 
